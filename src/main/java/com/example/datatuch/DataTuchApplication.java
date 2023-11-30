da6757c9a473b7b6bcf85bf675b2d2e7838f94c1
KeKeiGenkai <stalker22463@gmail.com>
2023-11-30 22:37:00 +0300

    Добаленна возможность выбора Всё разделено по своим классам

--- src/main/java/com/example/datatuch/DataTuchApplication.java
+++ src/main/java/com/example/datatuch/DataTuchApplication.java
@@ -1,79 +1,39 @@
 package com.example.datatuch;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 @SpringBootApplication
 public class DataTuchApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DataTuchApplication.class, args);
-        String url = "jdbc:postgresql://localhost:5432/1613";
-        String user = "postgres";
-        String pass = "1488";
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        try (Connection connection = DriverManager.getConnection(url, user, pass);
-             Scanner scanner = new Scanner(System.in)) {
+        while (true) {
+            System.out.println("Выберите действие:");
+            System.out.println("1. Парсинг TG даты");
+            System.out.println("2. Поля регистрации");
+            System.out.println("3. Статистика");
 
-            System.out.print("Login:");
-            String login = scanner.nextLine();
+            String choiceString = reader.readLine();
+            int choice = Integer.parseInt(choiceString);
 
-            System.out.print("Password:");
-            String password = scanner.nextLine();
-
-            System.out.print("Name:");
-            String name = scanner.nextLine();
-
-            System.out.print("You age:");
-            int age = scanner.nextInt();
-            scanner.nextLine();
-
-            System.out.print("You country:");
-            String country = scanner.nextLine();
-
-            System.out.print("Phone number:");
-            long phonenum = scanner.nextLong();
-
-
-            String sql = "INSERT INTO \"users\" (login, password, phoneNum, age, country, name) VALUES (?, ?, ?, ?, ?, ?)";
-
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, login);
-                preparedStatement.setString(2, password);
-                preparedStatement.setLong(3, phonenum);
-                preparedStatement.setInt(4, age);
-                preparedStatement.setString(5, country);
-                preparedStatement.setString(6, name);
-
-                int rowsAffected = preparedStatement.executeUpdate();
-
-                System.out.println("Rows affected: " + rowsAffected);
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (connection != null) {
-                        connection.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
-                if (scanner != null) {
-                    scanner.close();
-                }
+            switch (choice) {
+                case 1:
+                    AppConfig.data();
+                    break;
+                case 2:
+                    reg.regist();
+                    break;
+                case 3:
+                    MyService.mostYear();
+                    break;
+                default:
+                    System.out.println("Некорректный выбор. Пожалуйста, выберите снова.");
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
-        AppConfig.data();
     }
-}
+    }
--- src/main/java/com/example/datatuch/MyService.java
+++ src/main/java/com/example/datatuch/MyService.java
@@ -2,9 +2,40 @@ package com.example.datatuch;
 
 import org.springframework.stereotype.Service;
 
+import java.sql.*;
+
 
 @Service
 public class MyService {
-    public void processUserInput(String userInput) {
+    public static void main(String[] args) {
+    }
+    public static void mostYear(){
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/1613", "postgres", "1488");
+            String sqlQuery = "SELECT EXTRACT(MONTH FROM datasend) AS month, COUNT(*) AS textmess " +
+                    "FROM telegramdata " +
+                    "WHERE EXTRACT(YEAR FROM datasend) = 2022 " +
+                    "GROUP BY EXTRACT(MONTH FROM datasend) " +
+                    "ORDER BY month";
+
+            try (PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    // Выводим заголовок таблицы
+                    System.out.printf("| %-10s | %-15s |\n", "Month", "Message Count");
+                    System.out.println("|------------|-----------------|");
+
+                    // Выводим результаты в консоль
+                    while (resultSet.next()) {
+                        int month = resultSet.getInt("month");
+                        int messageCount = resultSet.getInt("textmess");
+
+                        // Выводим строку таблицы
+                        System.out.printf("| %-10d | %-15d |\n", month, messageCount);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
